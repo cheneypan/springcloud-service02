319e065bc51ea8417276c1bffe5bd22032730a71
pan <pan@cloudtogo.cn>
2018-08-05 01:31:04 +0800

    service02

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -12,6 +13,7 @@ import java.util.Date;
 /**
  * Created by cheney on 2018/8/5.
  */
+@RestController
 public class IndexController {
 
     private Logger logger = LoggerFactory.getLogger(getClass());
