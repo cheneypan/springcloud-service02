13a8b5b71bbf378ed60da9edafc60bba3cf0030f
pan <pan@cloudtogo.cn>
2018-08-05 09:38:22 +0800

    service02

--- src/main/java/com/example/demo/client/Service01Client.java
+++ src/main/java/com/example/demo/client/Service01Client.java
@@ -1,15 +1,29 @@
 package com.example.demo.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by cheney on 2018/8/5.
  */
-@FeignClient(name = "service01")
+@FeignClient(serviceId = "service01", fallbackFactory = Service01Client.Service01ClientFallback.class)
 public interface Service01Client {
 
     @RequestMapping(value="/reply",method = RequestMethod.GET)
     String reply(@RequestParam("message") String message);
+
+    class Service01ClientFallback implements Service01Client {
+
+        private Logger logger = LoggerFactory.getLogger(getClass());
+
+        @Override
+        public String reply(@RequestParam("message") String message) {
+            logger.error("ERROR: param[message = {}]", message);
+            String reply = "ERROR -> " + message;
+            return reply;
+        }
+    }
 
 }
